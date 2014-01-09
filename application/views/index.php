a0d968fe1a483ef3e97213469d29286af81346c8
André Philip Kallehauge <andrekallehauge@gmail.com>
2014-01-09 22:32:28 +0100

    Example: added a stylesheet and script to illustrate function app./views/index.php

--- application/views/index.php
+++ application/views/index.php
@@ -1,1 +1,16 @@
-<p> This is some default data: <?= $default; ?>. </p>
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<!-- Stylesheet -->
+	<link rel="stylesheet" href="<?= base_url('public/css/style.css'); ?>">
+	<!-- Script -->
+	<script src="<?= base_url('public/js/script.js'); ?>"></script>
+
+</head>
+<body>
+	
+	<p> This is some default data: <?= $default; ?>. </p>
+
+</body>
+</html>
